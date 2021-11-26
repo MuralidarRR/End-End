f81e1170118397dd0bc34cdccc565639847b0551
Muralidar R R <muralidar.2001125@srec.ac.in>
2021-11-26 15:03:00 +0530

    Hate Speech

---
+++ app.py
@@ -0,0 +1,12 @@
+def hate_speech_detection():
+    import streamlit as st
+    st.title("Hate Speech Detection")
+    user = st.text_area("Enter any Tweet: ")
+    if len(user) < 1:
+        st.write("  ")
+    else:
+        sample = user
+        data = cv.transform([sample]).toarray()
+        a = clf.predict(data)
+        st.title(a)
+hate_speech_detection()
